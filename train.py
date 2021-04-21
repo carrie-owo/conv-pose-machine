f9f3ac3630e2f910de60a5e3a6f8b10f7bbd6375
Charjabug <48063425+Charjabug@users.noreply.github.com>
2021-04-21 16:00:28 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,24 +7,16 @@ from datetime import datetime
 from preprocess.gen_data import LSP_DATA
 from preprocess.Transformers import Compose, RandomCrop, RandomResized
 from model import CPMModel
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-resume = None
 log_file_path = './my_log.txt'
-weight_dir = pathlib.Path('weights').absolute()
-weights_file = weight_dir.joinpath('cpm.h5')
-#Arguments
-parser = argparse.ArgumentParser(description='CPM')
-parser.add_argument('--resume', default=None, type=str)
-args = parser.parse_args()
-if args.resume:
-    resume = args.resume
-
 
 learning_rate = 1e-4
-num_epoch = 1
+num_epoch = 5
 batch_size = 1
 save_interval = 100
-optimizer = keras.optimizers.SGD(learning_rate)
+optimizer = keras.optimizers.Adam(learning_rate)
 loss = keras.losses.MeanSquaredError()
 
 training_dataset_path = 'lspet_dataset'
@@ -37,9 +29,6 @@ def loss_function(y_true, y_pred):
 
     y_pred = tf.squeeze(y_pred, axis=1)
 
-    # print("y_true.shape: ", y_true.shape)
-    # print("y_pred.shape: ", y_pred.shape)
-
     loss0 = loss(y_true, y_pred[0])
     loss1 = loss(y_true, y_pred[1])
     loss2 = loss(y_true, y_pred[2])
@@ -48,70 +37,17 @@ def loss_function(y_true, y_pred):
     loss5 = loss(y_true, y_pred[5])
 
     total_loss = loss0 + loss1 + loss2 + loss3 + loss4 + loss5
-    # print("total_loss: ", total_loss)
     return total_loss
 
 def train():
-    # image_input = keras.Input(shape=image_shape)
-    # centermap_input = keras.Input(shape=centermap_shape)
     cpm = CPMModel()
-
-    # image_input = tf.squeeze(image_input, axis=0)
-    # centermap_input = tf.squeeze(centermap_input, axis=0)
-
-    # print("image_input.shape: ", image_input.shape)
-    # print("centermap_input.shape: ", centermap_input.shape)
-
-    # outputs = cpm(image_input, centermap_input)
-
-    # print("outputs.shape: ", outputs.shape)
-
-    # model = keras.Model(inputs=[image_input, centermap_input], outputs=outputs, name='CPMModel')
-    # model.compile(optimizer=optimizer, loss=loss_function, metrics=None)
-    # model.summary()
-
-    # with open(log_file_path, "a") as f:
-    #     f.write('\nTraining begins at: %s\n' % datetime.now())
-
-    # if resume:
-    #     print('Loading weights from %s' % resume)
-    #     model.load_weights(resume)
-
-    # # helper function to save state of model
-    # def save_weights():
-    #     print('Saving state of model to %s' % weights_file)
-    #     with open(log_file_path, "a") as f:
-    #         f.write('\nSaving state of model to %s' % weights_file)
-    #     model.save_weights(str(weights_file))
-
-    # # signal handler for early abortion to autosave model state
-    # def autosave(sig, frame):
-    #     print('Training aborted. Saving weights.')
-    #     save_weights()
-    #     exit(0)
-
-    # for sig in [signal.SIGABRT, signal.SIGINT, signal.SIGTSTP]:
-    #     signal.signal(sig, autosave)
 
     data = LSP_DATA('lspet', training_dataset_path, 8, Compose([RandomResized(), RandomCrop(368)]))
     print("---------- Start Training ----------")
     for e in range(num_epoch):
         l = len(data)
         num_error=0
-        # try:
-            # for i, d in enumerate(data):
-            #     if i%100==0:
-            #         print(i,l,i/l)
-            #     image, heatmap, centermap = d
-            #     image = tf.expand_dims(image, axis=0)
-            #     # image = tf.expand_dims(image, axis=0)
-            #     centermap = tf.expand_dims(centermap, axis=0)
-            #     # centermap = tf.expand_dims(centermap, -1)
-            #     print("image.shape: ", image.shape)
-            #     print("heatmap.shape: ", heatmap.shape)
-            #     print("centermap.shape: ", centermap.shape)
-            #     # loss = model.train_on_batch((image, centermap), heatmap)
-            #     loss = model.fit((image, centermap), heatmap)
+
         for i, d in enumerate(data):
             if d is None:
                 num_error += 1
@@ -126,12 +62,30 @@ def train():
                     output = cpm(image, centermap)
                     loss = loss_function(heatmap, output)
                 gradients = tape.gradient(loss, cpm.trainable_variables)
-                optimizer.apply_gradients(zip(gradients, cpm.trainable_variables))                   
-            except:
+                optimizer.apply_gradients(zip(gradients, cpm.trainable_variables)) 
+
+                # for j in range(15):
+                       
+                #     heat = heatmap[:,:, j]
+
+                #     plt.figure()
+                #     p1 = sns.heatmap(heat)
+                #     figure = p1.get_figure()
+                #     figure.savefig("o1" + str(j) + ".png", dpi=400) 
+
+                # heat = image[0,:,:,0]
+
+                # plt.figure()
+                # p1 = sns.heatmap(heat)
+                # figure = p1.get_figure()
+                # figure.savefig("o2" + str(i) + ".png", dpi=400)
+                # exit()
+                # break
+                # exit()             
+            except Exception as e:
+                # print(e)
                 num_error += 1
-        # except KeyboardInterrupt:
-        #     save_weights()
-        #     return    
+        # exit()
         print('\nTraining epoch {} with loss {}'.format(e, loss))
         print("num_error:",num_error)
         if e % 1 == 0:
@@ -139,11 +93,6 @@ def train():
             with open(log_file_path, "a") as f:
                 f.write('\n[%05d] Loss: %.4f' % (e, loss))
             cpm.save_weights(os.path.join("ck",str(e)+" "+str(float(loss))+" loss"))
-        # if save_interval and e > 0 and e % save_interval == 0:
-        #     save_weights()
-        
-        # with open(log_file_path, "a") as f:
-        #     f.write('\nCurrent time: %s\n' % datetime.now())
 
 
 if __name__ == "__main__":
